522729030800042d21f100153c51b52df5481fb4
eduran2 <Eduardo@Eduardos-Mac.local>
2016-10-18 20:11:28 -0700

    formatted webpage layout

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ $('h1').lettering();
 //Make Backstretch GO!!
 $.backstretch('/img/bg.jpg');
 
+// Masonry
+$('main').masonry({
+  // options
+  itemSelector: 'section',
+  itemWidth: 100,
+  gutter: 10,
+  transitionDuration: '1s'
+});
